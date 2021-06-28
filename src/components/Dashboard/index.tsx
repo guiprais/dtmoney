a182029623d525e91aef522d596263ca429b9034
Guilherme de Prais <guilherme.prais@gmail.com>
2021-06-28 10:01:19 -0300

    feat: summary

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import { Summary } from '../Summary';
+
+import { Container } from './styles';
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Container>
+        <Summary />
+      </Container>
+    </div>
+  );
+}
