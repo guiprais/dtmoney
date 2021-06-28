a63158a808e5c1ec5fb6d5486ca3083ff65ebce1
Guilherme de Prais <guilherme.prais@gmail.com>
2021-06-28 10:18:29 -0300

    feat: transactions table

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import { Summary } from '../Summary';
+import TransactionsTable from '../TransactionsTable';
 
 import { Container } from './styles';
 
@@ -7,6 +8,7 @@ export default function Dashboard() {
     <div>
       <Container>
         <Summary />
+        <TransactionsTable />
       </Container>
     </div>
   );
