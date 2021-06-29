b6e091cda187087aec382c57e0f863a6e94ac18a
Guilherme de Prais <guilherme.prais@gmail.com>
2021-06-28 22:48:07 -0300

    feat: add axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
+import { api } from '../../services/api';
 import { Container } from './styles';
 
 export default function TransactionsTable() {
+  useEffect(() => {
+    api.get('transactions').then((response) => console.log(response.data));
+  }, []);
+
   return (
     <Container>
       <table>
@@ -16,13 +22,13 @@ export default function TransactionsTable() {
         <tbody>
           <tr>
             <td>Desenvolvimento de website</td>
-            <td className="deposit">R$12.000,00</td>
+            <td className="deposit">R$ 12.000,00</td>
             <td>Desenvolvimento</td>
             <td>20/02/2021</td>
           </tr>
           <tr>
             <td>Aluguel</td>
-            <td className="withdraw">- R$1.100,00</td>
+            <td className="withdraw">- R$ 1.100,00</td>
             <td>Casa</td>
             <td>17/02/2021</td>
           </tr>
